a313f9a254361de91bcfb58fbf5a0f6bfe21ec27
Matheus Rodrigues <matheus.rodrisantos@outlook.com>
2025-05-30 20:11:56 -0300

    Refactor RegistroPontoService to improve code clarity and maintainability; update method signature and streamline date handling

--- src/Sevice/RegistroPontoService.php
+++ src/Sevice/RegistroPontoService.php
@@ -2,14 +2,15 @@
 
 namespace App\Sevice;
 
+use App\Dto\RegistroPontoDTO;
 use App\Entity\RegistroPonto;
 use App\Entity\ValueObject\BatidaPonto;
 use App\Exception\RegraDeNegocioFuncionarioException;
 use App\Factory\RegistroPontoFactory;
 use App\Repository\FuncionarioRepository;
 use App\Repository\RegistroPontoRepository;
-use DateTime;
 use DateTimeImmutable;
+use DateTimeZone;
 
 class RegistroPontoService
 {
@@ -20,22 +21,19 @@ class RegistroPontoService
         private RegistroPontoFactory $registroPontoFactory
     ) {}
 
-    public function registrar(int $funcionarioID)
+    public function registrar(int $funcionarioID):RegistroPontoDTO
     {
-
         // TODO: Substituir busca por ID direto por busca baseada no usuário autenticado.
-
-        $funcionario = $this->funcionarioRepository->find(id: $funcionarioID);
+        //uma regra tem que ter um funcionario e ele deve estar ativo
+        $funcionario = $this->funcionarioRepository->buscarFuncionarioAtivoPorId(id: $funcionarioID);
 
         if ($funcionario == null) {
             throw new RegraDeNegocioFuncionarioException("Funcionario não encontrado");
         }
 
         $dataHora = new DateTimeImmutable(
-            datetime: "now",
-            timezone: new \DateTimeZone("America/Sao_Paulo")
+            timezone: new DateTimeZone("America/Sao_Paulo")
         );
-
         
         $registroPonto = $this->registroPontoRepository->procurarPorPontoAberto($dataHora, $funcionario);
 
@@ -46,12 +44,13 @@ class RegistroPontoService
             $registroPonto->atribuirFuncionario($funcionario);
         }
 
+        //uma regra é que o funcionario não pode bater mais de 10:00:00
         $registroPonto->baterPonto(dataHora: $dataHora);
 
         $this->registroPontoRepository->create($registroPonto);
 
+    
         $dto = $this->registroPontoFactory->createDto($registroPonto);
-
 
         return $dto;
     }
