84024622933dd5d914e527c8e28a42e660f27a2a
Matheus Rodrigues dos Santos <matheus.rodrisantos@outlook.com>
2025-06-12 10:21:24 -0300

    feat: Add RegraDeNegocioFeriadoException class for handling holiday business rule exceptions

--- src/Entity/Feriado.php
+++ src/Entity/Feriado.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Entity\Enum\FeriadoNivel;
+use App\Exception\RegraDeNegocioFeriadoException;
 use App\Repository\FeriadoRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -14,9 +15,6 @@ class Feriado
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
-    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
-    private ?\DateTimeImmutable $data = null;
 
     #[ORM\Column(enumType: FeriadoNivel::class)]
     private ?FeriadoNivel $nivel = null;
@@ -38,17 +36,6 @@ class Feriado
         return $this->id;
     }
 
-    public function getData(): ?\DateTimeImmutable
-    {
-        return $this->data;
-    }
-
-    public function setData(\DateTimeImmutable $data): static
-    {
-        $this->data = $data;
-
-        return $this;
-    }
 
     public function getNivel(): ?FeriadoNivel
     {
@@ -81,6 +68,21 @@ class Feriado
 
     public function setInicio(\DateTimeImmutable $inicio): static
     {
+        if ($this->inicio === null && $inicio < new \DateTimeImmutable()) {
+            throw new RegraDeNegocioFeriadoException("A data de fim não pode ser anterior à data atual.");
+        }
+        if ($this->fim !== null && $inicio > $this->fim) {
+            throw new RegraDeNegocioFeriadoException("A data de início não pode ser posterior à data de fim.");
+        }
+        if ($this->fim !== null && $inicio->diff($this->fim)->days > 365) {
+            throw new RegraDeNegocioFeriadoException("A data de início não pode ser mais de 365 dias antes da data de fim.");
+        }
+        if ($this->inicio !== null && $inicio->diff($this->inicio)->days > 365) {
+            throw new RegraDeNegocioFeriadoException("A data de início não pode ser mais de 365 dias após a data de início atual.");
+        }
+        if ($this->fim === null && $inicio->diff(new \DateTimeImmutable())->days > 365) {
+            throw new RegraDeNegocioFeriadoException("A data de início não pode ser mais de 365 dias após a data atual.");
+        }
         $this->inicio = $inicio;
 
         return $this;
@@ -88,11 +90,23 @@ class Feriado
 
     public function getFim(): ?\DateTimeImmutable
     {
+
         return $this->fim;
     }
 
     public function setFim(\DateTimeImmutable $fim): static
     {
+
+        if ($this->inicio !== null && $fim < $this->inicio) {
+            throw new RegraDeNegocioFeriadoException("A data de fim não pode ser anterior à data de início.");
+        }
+        if ($this->inicio !== null && $fim->diff($this->inicio)->days > 365) {
+            throw new RegraDeNegocioFeriadoException("A data de fim não pode ser mais de 365 dias após a data de início.");
+        }
+
+        if ($this->inicio === null && $fim->diff(new \DateTimeImmutable())->days > 365) {
+            throw new RegraDeNegocioFeriadoException("A data de fim não pode ser mais de 365 dias após a data atual.");
+        }
         $this->fim = $fim;
 
         return $this;
